bd97454895a479e2cf5dc301c5370b71dc0ba4b4
david_chen <ychendavid@gmail.com>
2020-01-17 11:54:46 +0800

    rewrite reward

--- RL-Trader.py
+++ RL-Trader.py
@@ -136,8 +136,9 @@ def choose_trade(pointer, q_table, inPortfolio):
     # If the greedy factor is less than a randomly distributed number, if there are no values
     # on the Q-table, or if less than half the possible trades have been run without our trading logic,
     # return our analytical trade logic decision
-    print np.count_nonzero(state_actions) == 0
-    if np.random.uniform() > float(agent.epsilon) or np.count_nonzero(state_actions) == 0 or pointer < int(TRADES_TO_RUN):
+    print q_table
+    print np.count_nonzero(q_table) == 0
+    if np.random.uniform() > float(agent.epsilon) or np.count_nonzero(q_table) == 0 or pointer < int(TRADES_TO_RUN):
         print "analytical"
         return analytic_decision
     # Otherwise, return what has been working
