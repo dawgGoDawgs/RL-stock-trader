b804f471d0514143de5384066c2d88fa654cea22
david_chen <ychendavid@gmail.com>
2020-01-17 16:24:32 +0800

    state variable

--- Logic/logic.py
+++ Logic/logic.py
@@ -7,27 +7,31 @@ from math import *
 # This is a simple boilerplate trading strategy that should be modified to yield better returns
 # ** If you are changing the trading strategy, keep 0 for Sell and 1 for Buy ** 
 def state_logic(pointer, data):
-    spot_price = data['EQUITY'][pointer]
-    rf_rate = data['RF'][pointer] if not np.isnan(data['RF'][pointer]) else 1.0 # default to 1% if nan
-    sigma = data['SIGMA'][pointer]
-    # BSM for if the option's exercise price appreciates by 5%
-    price_increase = calculate_BSM(spot_price,
-                                   spot_price * 1.05, # 5% appreciation
-                                   rf_rate / 100,
-                                   sigma / 100, 1 # Roughly one day timeframe
-                                   / 365.0)
-    # BSM for if the option's exercise price holds its value
-    stable_price = calculate_BSM(spot_price, spot_price, rf_rate / 100,
-                                 sigma / 100, 1
-                                 / 365.0)
-    # Tinker with this 
-    returns = log(stable_price / price_increase)
-    # Tinker with the return threshold as well
-    print "pointer:", pointer
-    if returns <= 10:
-        return 1  # sell
-    if returns > 10:
-        return 0  # buy
+    # spot_price = data['EQUITY'][pointer]
+    # rf_rate = data['RF'][pointer] if not np.isnan(data['RF'][pointer]) else 1.0 # default to 1% if nan
+    # sigma = data['SIGMA'][pointer]
+    # # BSM for if the option's exercise price appreciates by 5%
+    # price_increase = calculate_BSM(spot_price,
+    #                                spot_price * 1.05, # 5% appreciation
+    #                                rf_rate / 100,
+    #                                sigma / 100, 1 # Roughly one day timeframe
+    #                                / 365.0)
+    # # BSM for if the option's exercise price holds its value
+    # stable_price = calculate_BSM(spot_price, spot_price, rf_rate / 100,
+    #                              sigma / 100, 1
+    #                              / 365.0)
+    # # Tinker with this 
+    # returns = log(stable_price / price_increase)
+    # # Tinker with the return threshold as well
+    # print "pointer:", pointer
+    # if returns <= 10:
+    #     return 0  # buy
+    # if returns > 10:
+    #     return 1  # sell
+    if np.random.uniform() < 0.3:
+      return 0
+    else:
+      return 1
 
 # Black-Scholes Model function needed for our current state logic
 def calculate_BSM(
