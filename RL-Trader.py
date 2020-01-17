c7ecd101aed89144abff962ebf348187cbe6d369
david_chen <ychendavid@gmail.com>
2020-01-17 12:10:31 +0800

    rewrite reward

--- RL-Trader.py
+++ RL-Trader.py
@@ -141,6 +141,7 @@ def choose_trade(pointer, q_table, inPortfolio):
         return analytic_decision
     # Otherwise, return what has been working
     else:
+        print state_actions
         maximum = state_actions.idxmax()
         if str(maximum) == 'buy':
             return 0
