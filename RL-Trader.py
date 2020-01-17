43240f5c98a0133dd406c280b85942c156da7ba9
david_chen <ychendavid@gmail.com>
2020-01-17 20:19:31 +0800

    change option return

--- RL-Trader.py
+++ RL-Trader.py
@@ -219,7 +219,7 @@ def buildReward(n_periods, trade_prev, trade_cur, ret):
             return 0
     if trade_cur == 1:
         if trade_prev == 0:
-            return ret / n_periods
+            return ret
         if trade_prev == 1:
             return 0
 
