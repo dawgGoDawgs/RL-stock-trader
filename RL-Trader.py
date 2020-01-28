8cca127d66b7977a960e6821fd1a3ffbe43300bf
david_chen <ychendavid@gmail.com>
2020-01-28 08:58:51 -0500

    fix code

--- RL-Trader.py
+++ RL-Trader.py
@@ -253,10 +253,10 @@ def run():
         # Find the payoff from the trade
         ret, inPortfolio = determine_payoff(x, trade, inPortfolio)
         # track signle interval position ret at next
-        print(inPortfolio)
         if inPortfolio:
             position_ret_one_interval = (data['EQUITY'][x + 1] - data['EQUITY'][x]) / float(data['EQUITY'][x])
             position_ret_curve.append(position_ret_one_interval)
+            print(position_ret_curve)
         else:
             position_ret_curve = [] # end tracking return curve
         # construct next state
