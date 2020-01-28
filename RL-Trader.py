ccf0ca34d981e6aa6686dd16e1405df17d52d501
david_chen <ychendavid@gmail.com>
2020-01-28 09:32:59 -0500

    fix code

--- RL-Trader.py
+++ RL-Trader.py
@@ -102,7 +102,7 @@ model = GaussianHMM(n_components=num_components, covariance_type="diag", n_iter=
 hidden_states = model.predict(X)
 
 # Why not edit this?
-STATES = 20
+STATES = 12
 # Actions of Q-Table
 ACTIONS = ['buy', 'sell']
 # Holds total trades that can be made
@@ -181,17 +181,15 @@ def select_state(pointer, current_in_portfolio):
             state = 3 # Equity Appreciated and Hidden is 3
         if local_win_rate >= 0.5:
             state += 4
-        if position_ret >= 0:
-            state += 8
     else:
         if current_hidden == 0:
-            state = 16 # Equity Appreciated and Hidden is 0
+            state = 8 # Equity Appreciated and Hidden is 0
         if current_hidden == 1:
-            state = 17 # Equity Appreciated and Hidden is 1
+            state = 9 # Equity Appreciated and Hidden is 1
         if current_hidden == 2:
-            state = 18 # Equity Appreciated and Hidden is 2
+            state = 10 # Equity Appreciated and Hidden is 2
         if current_hidden == 3:
-            state = 19 # Equity Appreciated and Hidden is 3
+            state = 11 # Equity Appreciated and Hidden is 3
 
     return state
 # Function to find the profit from trades
@@ -315,22 +313,14 @@ Q-table:
 '''
     # Add reference column
     q_table["Reference"] = [
-        'Hidden is 0 and in portfolio and local win rate >= 0.5 and position ret >= 0',
-        "Hidden is 1 and in portfolio and local win rate >= 0.5 and position ret >= 0",
-        "Hidden is 2 and in portfolio and local win rate >= 0.5 and position ret >= 0",
-        "Hidden is 3 and in portfolio and local win rate >= 0.5 and position ret >= 0",
-        'Hidden is 0 and in portfolio and local win rate < 0.5 and position ret >= 0',
-        "Hidden is 1 and in portfolio and local win rate < 0.5 and position ret >= 0",
-        "Hidden is 2 and in portfolio and local win rate < 0.5 and position ret >= 0",
-        "Hidden is 3 and in portfolio and local win rate < 0.5 and position ret >= 0",
-        'Hidden is 0 and in portfolio and local win rate >= 0.5 and position ret < 0',
-        "Hidden is 1 and in portfolio and local win rate >= 0.5 and position ret < 0",
-        "Hidden is 2 and in portfolio and local win rate >= 0.5 and position ret < 0",
-        "Hidden is 3 and in portfolio and local win rate >= 0.5 and position ret < 0",
-        'Hidden is 0 and in portfolio and local win rate < 0.5 and position ret < 0',
-        "Hidden is 1 and in portfolio and local win rate < 0.5 and position ret < 0",
-        "Hidden is 2 and in portfolio and local win rate < 0.5 and position ret < 0",
-        "Hidden is 3 and in portfolio and local win rate < 0.5 and position ret < 0",
+        'Hidden is 0 and in portfolio and local win rate >= 0.5',
+        "Hidden is 1 and in portfolio and local win rate >= 0.5",
+        "Hidden is 2 and in portfolio and local win rate >= 0.5",
+        "Hidden is 3 and in portfolio and local win rate >= 0.5",
+        'Hidden is 0 and in portfolio and local win rate < 0.5',
+        "Hidden is 1 and in portfolio and local win rate < 0.5",
+        "Hidden is 2 and in portfolio and local win rate < 0.5",
+        "Hidden is 3 and in portfolio and local win rate < 0.5",
         'Hidden is 0 and not in portfolio',
         "Hidden is 1 and not in portfolio",
         "Hidden is 2 and not in portfolio",
